87913254745aec946609b3713b7c0935ec74128f
Patrick GENDRE <patgendre94@gmail.com>
2020-04-14 09:45:33 +0200

    Update statsRD.py

--- statsRD.py
+++ statsRD.py
@@ -22,27 +22,9 @@ def statsRD(jour="2020-04-02",heure="10-30",rdc="rdc_0"):
 	ext100=[x[-4:]=="_100" for x in RD]
 	ext200=[x[-4:]=="_200" for x in RD]
 	extbad=[x[-4:]==".bad" for x in RD]
-	extbadlx=[x[6:13]=="_BadIx_" for x in RD]
+	extbadix=[x[6:13]=="_BadIx_" for x in RD]
 	lines=[len(open(path+"/"+x).readlines()) for x in RD]
-	df = pd.DataFrame({'pasRD': pasRD, 'ext100': ext100, 'ext200': ext200,
+	df = pd.DataFrame({'sansPAS': sansPAS, 'pasRD': pasRD, 'ext100': ext100, 'ext200': ext200,
 		'extbad': extbad, 'extbadix': extbadix, 'lines': lines})
 	return  df
-
-
-def statsRD(jour="2020-04-02",heure="10-30"):
-	for rep in os.listdir(jour):
-		RD=os.listdir(jour+"/"+rep)
-		n=len(RD)
-		#n*jour
-		path=jour+"/"+rep+"/"
-		sansPAS="RD_PAS" not in RD
-		is not sansPAS:
-			with open(jour+"/"+RD+"/RD_PAS","r") as RD_PAS
-				sansPAS=readline(RD_PAS)
-		pasRD=[x[0:2]!="RD" for x in RD]
-		ext100=[x[-4:]=="_100" for x in RD]
-		ext200=[x[-4:]=="_200" for x in RD]
-		extbad=[x[-4:]==".bad" for x in RD]
-		extbadix=[x[6:13]=="_Badlx" for x in RD]
-		lines=[len(open(path+x).readlines() for x in RD)]
 
